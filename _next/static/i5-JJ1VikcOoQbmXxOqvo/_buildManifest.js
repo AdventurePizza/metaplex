7f93fdf3ccffe559772104b1a266276522ac7b38
tanerdurmaz <taner.durmaz.cs@gmail.com>
2021-09-29 15:43:39 +0300

    Updates

---
+++ _next/static/i5-JJ1VikcOoQbmXxOqvo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-8b3040d0922513e61af1.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
